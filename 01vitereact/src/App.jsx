c49bb504ca90279dda8d40aa54359db3692c2794
Piyush Kumar Shukla <piyushshuklait@gmail.com>
2023-09-16 20:36:51 +0530

    first commit

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,10 @@
+
+
+function App() {
+
+  return (
+    <h1>React aur chai | Piyush Kumar shukla</h1>
+  )
+}
+
+export default App
---
+++ 02counter/src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [counter, setCounter] = useState(0)
+  const addValue = () =>{
+    setCounter(counter + 1)
+  }
+  const removeValue = () => {
+    if(counter > 0){
+      setCounter(counter - 1)
+    }
+  }
+  return (
+    <>
+      <h1>React aur chai</h1>
+      <h2>Counter value : {counter}</h2>
+      <button onClick={addValue}>add value</button>
+      <br/>
+      <br/>
+      <button onClick={removeValue}>Remove Value</button>
+    </>
+  )
+}
+
+export default App
